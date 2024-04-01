5d6181b33493f49319044facffe6cada2264e24c
Frozen owner <156554454+prashantsahlot@users.noreply.github.com>
2024-04-01 12:37:28 +0530

    Update ping.py

--- VIPMUSIC/plugins/tools/ping.py
+++ VIPMUSIC/plugins/tools/ping.py
@@ -102,10 +102,10 @@ async def ping_com(client, message: Message, _):
         ],
         [
             InlineKeyboardButton(
-                text="✦ ɢʀᴏᴜᴘ ✦", url=f"https://t.me/TG_FRIENDSS",
+                text="✦ ɢʀᴏᴜᴘ ✦", url=f"https://t.me/Frozensupport1",
             ),
             InlineKeyboardButton(
-                text="✧ ᴍᴏʀᴇ ✧", url=f"https://t.me/VIP_CREATORS",
+                text="✧ ᴍᴏʀᴇ ✧", url=f"https://t.me/frozenupdates1",
             )
         ],
         [
